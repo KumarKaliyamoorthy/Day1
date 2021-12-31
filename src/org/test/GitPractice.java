c4221c24a822b79cb691a48d4b8705dcc0ae1892
unknown <kumarkaliyamoorthy97@gmail.com>
2021-12-31 16:22:39 +0530

    username functionality

--- src/org/test/GitPractice.java
+++ src/org/test/GitPractice.java
@@ -1,5 +1,12 @@
 package org.test;
 
 public class GitPractice {
+	public static void kumar() {
+		System.out.println("welcome");
+
+	}
+	public static void main(String[] args) {
+		kumar();
+	}
 
 }
